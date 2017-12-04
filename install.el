5f1fcf77d4d74488a3ae6ca3d085a03b606ef6cf
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-12-04 12:49:14 +0000

    added packages installation script

---
+++ install.el
@@ -0,0 +1,15 @@
+(require 'package)
+
+; find package information from following archives
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+(package-initialize)
+(package-refresh-contents)
+
+(mapcar (lambda (package)
+					; install package if not already installed
+          (unless (package-installed-p package)
+            (package-install package)))
+	
+        '(magit intero helm markdown-mode multiple-cursors expand-region yasnippet color-theme))
